af283e4d5922904661fde35cbf55046e03d62afe
José Romero <jdromerov88@gmail.com>
2019-10-23 03:26:29 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,8 +11,5 @@ end
 def print_first_directors_movie_titles
   #pp directors_database[0][:movies]
   movies = directors_database[0][:movies]
-  i = 0
-  while i < movies.count do
-    puts movies[i][:title]
-  end
+
 end
