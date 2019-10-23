d9c16b0033dca7dcb3aaa53e5409d5c9d12e7ca8
José Romero <jdromerov88@gmail.com>
2019-10-23 03:00:44 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  pp directors_database[0][:movies]
+  #pp directors_database[0][:movies]
   movies = directors_database[0][:movies]
   i = 0
   while i < movies.count do
